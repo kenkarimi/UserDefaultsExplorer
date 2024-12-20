5db244f41678b5a42a9998336aca4001d2b015f2
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-12-20 17:58:30 +0300

    Initial Commit

---
+++ UserDefaultsExplorer/UserDefaultsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsExplorerApp.swift
+//  UserDefaultsExplorer
+//
+//  Created by Kennedy Karimi on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserDefaultsExplorerTests/UserDefaultsExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsExplorerTests.swift
+//  UserDefaultsExplorerTests
+//
+//  Created by Kennedy Karimi on 20/12/2024.
+//
+
+import Testing
+@testable import UserDefaultsExplorer
+
+struct UserDefaultsExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
